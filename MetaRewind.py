7a0a5a083700e7e125b6efcbff84660ae906bb73
Lasantha Sanjeewa <lasan.sudeera@gmail.com>
2025-08-14 21:58:01 +0530

    Updated UI

--- MetaRewind.py
+++ MetaRewind.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import Qt
+from PyQt6 import uic
+
+class MetaRewindApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui/main.ui", self)
+        
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    myApp = MetaRewindApp()
+    myApp.show()
+    
+    try:
+        sys.exit(app.exec())
+    except SystemExit:
+        print("Closing Window...")
+    
+    
