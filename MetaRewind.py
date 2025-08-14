aeae5106ad4419c939037b30bfb505a7b4adc192
Lasantha Sanjeewa <lasan.sudeera@gmail.com>
2025-08-14 22:46:04 +0530

    Added button icons

--- MetaRewind.py
+++ MetaRewind.py
@@ -3,6 +3,7 @@ import pandas as pd
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt6.QtCore import Qt
 from PyQt6 import uic
+from PyQt6.QtGui import QIcon
 
 from lightweight_charts.widgets import QtChart
 
@@ -28,6 +29,29 @@ class MetaRewindApp(QMainWindow):
         
         # Add chart's webview to the layout
         chart_layout.addWidget(self.chart.get_webview())
+        
+        # Set icons for toolbar buttons
+        self.setToolButtonIcons()
+    
+    def setToolButtonIcons(self):
+        """Set icons for all tool buttons"""
+        # Define icon paths
+        icon_paths = {
+            'btnAutoPlay': 'ui/icons/play_arrow.svg',
+            'BtnPause': 'ui/icons/pause.svg',
+            'btnFastSpeed': 'ui/icons/fast_forward.svg',
+            'btnPrevious': 'ui/icons/skip_previous.svg',
+            'btnNext': 'ui/icons/skip_next.svg',
+            'toolButton': 'ui/icons/fast_rewind.svg'  # Assuming this is for rewind
+        }
+        
+        # Set icons for each button
+        for button_name, icon_path in icon_paths.items():
+            if hasattr(self, button_name):
+                button = getattr(self, button_name)
+                button.setIcon(QIcon(icon_path))
+                # Make icon larger and more visible
+                button.setIconSize(button.size() * 0.4)
 
 
 if __name__ == "__main__":
