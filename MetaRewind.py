12e701dfe0bd634f33b81620a761a56dbe1d8d61
Lasantha Sanjeewa <lasan.sudeera@gmail.com>
2025-08-14 22:05:56 +0530

    WIP

--- MetaRewind.py
+++ MetaRewind.py
@@ -1,12 +1,17 @@
 import sys
+import pandas as pd
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtCore import Qt
 from PyQt6 import uic
+
+from lightweight_charts.widgets import QtChart
 
 class MetaRewindApp(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui/main.ui", self)
+        
+        chart = QtChart(self)
         
         
 if __name__ == "__main__":
